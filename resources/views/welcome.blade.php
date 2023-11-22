17a0efa695c86ff2103b35625b87aec3c72b7d8d
romanlazko <romanlazko@gmail.com>
2023-11-22 18:42:47 +0100

    Проверка гипотизы

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <script   src="https://code.jquery.com/jquery-3.7.1.js"   integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="   crossorigin="anonymous"></script>
+        {{-- <script   src="https://code.jquery.com/jquery-3.7.1.js"   integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="   crossorigin="anonymous"></script> --}}
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="">
