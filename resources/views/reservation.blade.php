af91db04b7dd8b014e2c3affadb1a8c02fdd48cc
romanlazko <romanlazko@gmail.com>
2023-12-22 13:14:49 +0100

    Исправление багов

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -1,1 +1,16 @@
-<iframe class="w-full min-h-[80vh]" src="https://webapp.buukan.com/1"></iframe>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" style="scroll-behavior: smooth;">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>BeautyBar</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    </head>
+    <body class="">
+        <iframe class="w-full min-h-[80vh]" src="https://webapp.buukan.com/1"></iframe>
+    </body>
+</html>
