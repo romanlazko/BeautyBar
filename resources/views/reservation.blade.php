1f8f1ccadd458d0d1fa3eadc88e28475f6ca0392
romanlazko <romanlazko@gmail.com>
2023-12-22 13:15:33 +0100

    Исправление размеров

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     </head>
-    <body class="">
+    <body class="w-full h-screen">
         <iframe class="w-full min-h-[80vh]" src="https://webapp.buukan.com/1"></iframe>
     </body>
 </html>
